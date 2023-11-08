6f615316a4d61a5ade107b9fe8fa4f6a25a9daaf
Rodrigo Mesquita <rodrigo.mesquita96@hotmail.com>
2023-11-07 21:51:57 -0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import { Home } from './src/screens/Home'
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  })
+
+  if (!fontsLoaded) {
+    return null
+  }
+
+  return <Home />
+}
